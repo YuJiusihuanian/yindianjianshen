01c23f9ea1a6186d6b4d0c5414e0ac7843505892
773714759@qq.com <773714759@qq.com>
2017-08-25 13:57:26 +0800

    改动路由

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <mt-button type="default">default</mt-button>
     <mt-button type="primary">primary</mt-button>
-    <mt-button type="danger">danger</mt-button>
+    <mt-button type="danger">测试页面</mt-button>
   </div>
 
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -9,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'AddictionCard',
+      component: AddictionCard
     },
     {
       path: '/addictionCard',
